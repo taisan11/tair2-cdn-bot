8a583e518213876c40e64ea2ffe9f3ed68538612
taisan11 <131413638+taisan11@users.noreply.github.com>
2025-07-08 17:16:24 +0900

    dekita!!

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,59 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.jsonc`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import { Button, Components, DiscordHono,Embed } from 'discord-hono'
 
-export default {
-	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
-	},
-} satisfies ExportedHandler<Env>;
+interface Bindings {
+  API:Service
+  APIKEY:string
+}
+
+const app = new DiscordHono<{Bindings:Bindings}>()
+  .command("アップロード",async (c)=>{
+    const name = crypto.randomUUID()
+    if (!c.env.APIKEY) {
+      throw new Error("APIKEY is undefined");
+    }
+    if (!name) {
+      throw new Error("name is undefined");
+    }
+    const res = await c.env.API.fetch("https://cdn.taisan11.f5.si/api/uploadkey",{
+      method: "POST",
+      body: JSON.stringify({
+        key: c.env.APIKEY,
+        name: name,
+      })
+    })
+    return c.flags("EPHEMERAL").res({
+      content: "このリンクからアップロードしてください。\nhttps://cdn.taisan11.f5.si/upload/" + name + "\n"+await res.text(),
+      components: new Components().row(
+        new Button("uploaded","アップロード完了したら押してください").custom_id(name),new Button("uploaded-video","動画をアップロードしたら押してください。").custom_id(name)
+      )
+    })
+  })
+  .component("uploaded", async (c) => {
+    return c.res({
+      content: "アップロードが完了しました。",
+      embeds: [
+        new Embed()
+          .title("ファイルアップロード完了")
+          .description("ファイルのアップロードが完了しました。")
+          .url("https://cdn.taisan11.f5.si/download/" + c.var.custom_id)
+          .color(0x00ff00)
+      ],
+      components: []
+    });
+  })
+  .component("uploaded-video", async (c) => {
+    return c.res({
+      content: "動画のアップロードが完了しました。",
+      embeds: [
+        new Embed()
+          .title("動画アップロード完了")
+          .description("動画のアップロードが完了しました。")
+          .url("https://cdn.taisan11.f5.si/download/" + c.var.custom_id)
+          .video({url:"https://cdn.taisan11.f5.si/download/" + c.var.custom_id})
+          .color(0x00ff00)
+      ],
+      components: []
+    });
+  })
+
+export default app
